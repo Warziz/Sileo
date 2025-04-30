00d30122d062a86ba1ab860bf8daeb342dfba192
Warziz <damien.lagrange2@gmail.com>
2025-04-30 11:38:19 +0200

    Edit agent.py: Add username et argparser

--- agent.py
+++ agent.py
@@ -1,5 +1,14 @@
 import socket
 import threading
+import argparse
+import pyfiglet
+
+from utils.user import generate_username
+
+def print_sileo():
+    ascii_art = pyfiglet.figlet_format("Sileo", font="slant")
+    print(ascii_art)
+
 
 def listener(host, port):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,32 +22,48 @@ def listener(host, port):
         data = conn.recv(1024)
         if not data:
             break
-        print(f"\nMessage reçu de {addr}: {data.decode('utf-8')}")
+        print(f"\nAnonymeUser>: {data.decode('utf-8')}")
     conn.close()
 
-def sender(target_host, target_port):
+def sender(target_host:str, target_port:int, username:str):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((target_host, target_port))
     print(f"Connecté à {target_host}:{target_port}")
 
     while True:
-        msg = input(">> ")
+        msg = input(f"{username}>> ")
         if msg.lower() == "exit":
             break
         client.send(msg.encode('utf-8'))
     client.close()
 
-if __name__ == "__main__":
-        
-    # Exemple : 192.168.1.2, ports 1500 (envoi), 1501 (réception)
-    local_host = '172.30.160.1'
-    #send_port = 1500
+def get_local_ip():
+    hostname = socket.gethostname()
+    local_host = socket.gethostbyname(hostname)
+    print(f"Your local adresse IP: {local_host}")
+    return local_host
+
+
+def main(target_ip):
+    
+    print_sileo()
+    username = generate_username()
+    
+    local_host = get_local_ip() #Récupère l'ip local
+    #local_host = '172.30.160.1'
     recv_port = 1501
 
     # Lancer l'écouteur dans un thread
     threading.Thread(target=listener, args=(local_host, recv_port), daemon=True).start()
 
     # Envoi des messages
-    target_host = '172.30.160.68'  # IP de la machine distante
+    target_host = '172.30.160.68'
     target_port = 1501         # port distant d'écoute
-    sender(target_host, target_port)
+    sender(target_ip, target_port, username) #Se connecte à la machine distante sur le port 1501
+    
+if __name__ == "__main__":
+        
+    parser = argparse.ArgumentParser(prog='agent.py')
+    parser.add_argument("target_ip", type=str, help="The IP you want to connect for send message")
+    args = parser.parse_args()
+    main(args.target_ip)
