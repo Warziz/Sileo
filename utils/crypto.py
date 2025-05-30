09b4ea3cd71541e1f615cb8d37014afe90ffba45
Damien L <damien.lagrange@erium.fr>
2025-05-30 16:41:44 +0200

    AES implementation test 5

--- utils/crypto.py
+++ utils/crypto.py
@@ -47,14 +47,14 @@ class DiffieHellman:
 class Cipher:
 
     @staticmethod
-    def encrypt_message(self,aes_key: bytes, message: str) -> bytes:
+    def encrypt_message(aes_key: bytes, message: str) -> bytes:
         aesgcm = AESGCM(aes_key)
         nonce = os.urandom(12)
         ciphertext = aesgcm.encrypt(nonce, message.encode(), None)
         return nonce + ciphertext
 
     @staticmethod
-    def decrypt_message(self, aes_key: bytes, data: bytes) -> str:
+    def decrypt_message(aes_key: bytes, data: bytes) -> str:
         aesgcm = AESGCM(aes_key)
         nonce = data[:12]
         ciphertext = data[12:]
