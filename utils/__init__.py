dd6c2632277cfa669841ad729d00a3b099f9a319
Warziz <damien.lagrange2@gmail.com>
2026-01-13 10:24:29 +0100

    Deleting old python files

--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import generate_username as generate_username
-from .user import color_text as color_text
-from .network import NetworkManager as NetworkManager
-from .arg import arguments as arguments
-from .crypto import DiffieHellman as DiffieHellman
